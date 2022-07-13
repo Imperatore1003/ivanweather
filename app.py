3e46210c483dd5ae3066ee5f375c418195999747
Ivan Beltrame <ivanbeltrame2007@gmail.com>
2022-07-13 18:20:48 +0200

    ssl

--- app.py
+++ app.py
@@ -12,6 +12,13 @@ app.config["SESSION_PERMANENT"] = False
 app.config["SESSION_TYPE"] = "filesystem"
 Session(app)
 
+@app.before_request
+def before_request():
+    if not request.is_secure:
+        url = request.url.replace('http://', 'https://', 1)
+        code = 301
+        return redirect(url, code=code)
+
 @app.after_request
 def after_request(response):
     """Ensure responses aren't cached"""
