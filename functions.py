031e6c82ca9e06d4c2f8581d66c236c6c598191e
Ivan Beltrame <ivanbeltrame2007@gmail.com>
2022-07-21 14:21:03 +0200

    Change text of the 404 error page

--- functions.py
+++ functions.py
@@ -21,27 +21,27 @@ def recentCities(cities):
 
 def api(city, cord1 = 0, cord2 = 0, mode = 0, unit = 0):
 
-    city = json.loads("{'coord': {'lon': 8.3213, 'lat': 40.5587}, 'weather': [{'id': 800, 'main': 'Clear', 'description': 'clear sky', 'icon': '01n'}], 'base': 'stations', 'main': {'temp': 28.01, 'feels_like': 28.55, 'temp_min': 28.01, 'temp_max': 28.01, 'pressure': 1016, 'humidity': 51}, 'visibility': 10000, 'wind': {'speed': 1.54, 'deg': 80}, 'clouds': {'all': 0}, 'dt': 1658263786, 'sys': {'type': 1, 'id': 6717, 'country': 'IT', 'sunrise': 1658203902, 'sunset': 1658256834}, 'timezone': 7200, 'id': 3183284, 'name': 'Alghero', 'cod': 200}".replace("'", '"'))
+    # city = json.loads("{'coord': {'lon': 8.3213, 'lat': 40.5587}, 'weather': [{'id': 800, 'main': 'Clear', 'description': 'clear sky', 'icon': '01n'}], 'base': 'stations', 'main': {'temp': 28.01, 'feels_like': 28.55, 'temp_min': 28.01, 'temp_max': 28.01, 'pressure': 1016, 'humidity': 51}, 'visibility': 10000, 'wind': {'speed': 1.54, 'deg': 80}, 'clouds': {'all': 0}, 'dt': 1658263786, 'sys': {'type': 1, 'id': 6717, 'country': 'IT', 'sunrise': 1658203902, 'sunset': 1658256834}, 'timezone': 7200, 'id': 3183284, 'name': 'Alghero', 'cod': 200}".replace("'", '"'))
 
-    # units = "imperial" if unit == 0 else "metric"
+    units = "imperial" if unit == 0 else "metric"
 
-    # prefix = ""
-    # preprefix = "https://api.openweathermap.org/data/2.5/weather?"
-    # suffix = "&appid=cfdec8014b5fefbdaeb3c9793299b335"
+    prefix = ""
+    preprefix = "https://api.openweathermap.org/data/2.5/weather?"
+    suffix = "&appid=cfdec8014b5fefbdaeb3c9793299b335"
 
-    # if mode == 0:
-    #     city = urllib.parse.quote(city)
+    if mode == 0:
+        city = urllib.parse.quote(city)
 
-    #     prefix = f"q={city}"
-    # elif mode == 1:
-    #     prefix = f"lat={cord1}&lon={cord2}"
+        prefix = f"q={city}"
+    elif mode == 1:
+        prefix = f"lat={cord1}&lon={cord2}"
 
-    # myAPI = preprefix + prefix + f"&lang=en&units={units}" + suffix
-    # myAPI = preprefix + prefix + f"&lang=en&units=metric" + suffix
+    myAPI = preprefix + prefix + f"&lang=en&units={units}" + suffix
+    myAPI = preprefix + prefix + f"&lang=en&units=metric" + suffix
 
-    # city = {}
-    # with urllib.request.urlopen(myAPI) as url:
-    #     city = json.loads(url.read().decode())
+    city = {}
+    with urllib.request.urlopen(myAPI) as url:
+        city = json.loads(url.read().decode())
 
     # print(str(city))
 
